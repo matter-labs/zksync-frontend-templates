0be3a171f81b0bf0b2638909aa600c33f5620a0d
Ihar Ko <ihqrk@proton.me>
2024-08-07 20:14:11 +0200

    styles update

--- templates/react/vite-web3js/src/main.tsx
+++ templates/react/vite-web3js/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
-import './index.css';
+import './styles/globals.css';
 
 BigInt.prototype.toJSON = function () {
   return this.toString();
