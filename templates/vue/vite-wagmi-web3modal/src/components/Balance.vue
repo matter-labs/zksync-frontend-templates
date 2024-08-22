f9de309a7deaede124dd34f5cd7c03b8a5336cf2
Jen <60794961+jennyg0@users.noreply.github.com>
2024-08-22 16:03:23 -0400

    formatted balance

--- templates/vue/vite-wagmi-web3modal/src/components/Balance.vue
+++ templates/vue/vite-wagmi-web3modal/src/components/Balance.vue
@@ -2,7 +2,7 @@
   <div>
     <div>
       Connected wallet balance:
-      {{ balance?.formatted }}
+      {{ formattedBalance }}
       <button @click="getAccountBalance">refetch</button>
     </div>
     <div v-if="error">Error: {{ error?.message }}</div>
@@ -10,15 +10,24 @@
 </template>
 
 <script lang="ts" setup>
-import { watch } from "vue"
+import { ref, watch } from "vue";
 import { getBalance as wagmiFetchBalance } from '@wagmi/core';
-
+import { wagmiConfig } from '../wagmi'; 
+import { formatUnits } from 'viem'; 
 import { useAsync } from '@/composables/useAsync';
 import { account } from '@/wagmi';
-import { wagmiConfig } from '../wagmi'; 
 
-const { result: balance, execute: fetchBalance, error} = useAsync(wagmiFetchBalance);
-const getAccountBalance = () => fetchBalance(wagmiConfig,{ address: account.value.address! });
+const { execute: fetchBalance, error } = useAsync(wagmiFetchBalance);
+
+const formattedBalance = ref<string | null>(null);
+
+const getAccountBalance = () => {
+  fetchBalance(wagmiConfig, { address: account.value.address! }).then(result => {
+    if (result) {
+      formattedBalance.value = formatUnits(result.value, result.decimals); 
+    }
+  });
+};
 
 watch(account, ({ address }) => {
   if (!address) return;
--- templates/vue/vite-wagmi-web3modal/src/components/FindBalance.vue
+++ templates/vue/vite-wagmi-web3modal/src/components/FindBalance.vue
@@ -5,7 +5,7 @@
       <input v-model="address" type="text" placeholder="wallet address">
       <button @click="() => fetchBalance(wagmiConfig, { address: address as Address })">{{ inProgress ? 'fetching...' : 'fetch' }}</button>
     </div>
-    <div>{{ balance?.formatted }}</div>
+      <div>{{ balance ? formatUnits(balance.value, balance.decimals): "" }}</div>
     <div v-if="error">Error: {{ error?.message }}</div>
   </div>
 </template>
@@ -15,10 +15,11 @@ import { ref } from "vue"
 import { getBalance } from '@wagmi/core';
 
 import { useAsync } from '@/composables/useAsync';
-import { Address } from "viem";
-import { wagmiConfig } from '../wagmi'; 
+import { wagmiConfig } from '../wagmi';
+import { formatUnits, type Address } from "viem";
 
 const address = ref("");
 
 const { result: balance, execute: fetchBalance, inProgress, error} = useAsync(getBalance);
+
 </script>
